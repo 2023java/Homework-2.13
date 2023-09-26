42a62ceaced7174941cf196160da13f5c2a370ce
Artnes <2023java@mail.ru>
2023-09-26 13:59:36 +0300

    Домашнее задание 2.13 - начало

---
+++ skyprospringmockito/src/main/java/pro/sky/skyprospringmockito/SkyprospringmockitoApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprospringmockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyprospringmockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyprospringmockitoApplication.class, args);
+	}
+
+}
